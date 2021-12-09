7bfb9eeffa92acd869b62bc67a3ff3c5189f57a9
daniil789 <pechenkin031199@mail.ru>
2021-12-09 16:26:41 +0500

    Add project files.

---
+++ PizzaOnLine/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using PizzaOnLine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaOnLine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            PizzaContext db = new PizzaContext();   
+
+            IEnumerable<Pizza> pizzas = db.Pizzas;
+            ViewBag.Pizzas = pizzas;
+
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PizzaOnLine/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PizzaOnLine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaOnLine.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+}
