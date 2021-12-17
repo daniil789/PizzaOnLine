2335c68482f7bde129e9fe8997a77a094984ea04
Daniil <pechenkin031199@mail.ru>
2021-12-17 15:45:12 +0500

    img

--- PizzaOnLine/Models/Pizza.cs
+++ PizzaOnLine/Models/Pizza.cs
@@ -11,6 +11,7 @@ namespace PizzaOnLine.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public string PathToImg { get; set; }
 
     }
 }
--- PizzaOnLine/Models/PizzaDbInitializer.cs
+++ PizzaOnLine/Models/PizzaDbInitializer.cs
@@ -10,9 +10,9 @@ namespace PizzaOnLine.Models
     {
         protected override void Seed(PizzaContext db)
         {
-            db.Pizzas.Add(new Pizza { Name = "Пепперони", Description = "Cыры Моцарелла и Пармезан, шампиньоны, бекон, колбаса пепперони, помидоры, куриная грудка, чеснок, лук красный, зелень.", Price = 220 });
-            db.Pizzas.Add(new Pizza { Name = "Маргарита", Description = "Cыр Моцарелла, помидоры.", Price = 200 });
-            db.Pizzas.Add(new Pizza { Name = "Гавайская", Description = "Cыр Моцарелла, ветчина, ананасы.", Price = 200 });
+            db.Pizzas.Add(new Pizza { Name = "Пепперони", Description = "Cыры Моцарелла и Пармезан, шампиньоны, бекон, колбаса пепперони, помидоры, куриная грудка, чеснок, лук красный, зелень.", Price = 220, PathToImg = "/Content/Pepperoni.jpg" });
+            db.Pizzas.Add(new Pizza { Name = "Маргарита", Description = "Cыр Моцарелла, помидоры.", Price = 200 , PathToImg = "/Content/Margarita.jpg" });
+            db.Pizzas.Add(new Pizza { Name = "Гавайская", Description = "Cыр Моцарелла, ветчина, ананасы.", Price = 200 , PathToImg = "/Content/Нawaiian.jpg" });
 
             base.Seed(db);
         }
